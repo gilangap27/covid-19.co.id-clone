a1e34f7edb634013bc602bbe6a2c06ddb1a1d0ee
gilangap27 <harzthdev01@gmail.com>
2021-09-16 20:47:09 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        open: ['Open Sans']
+      },
+      colors: {
+        secondary: "#191F62",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
